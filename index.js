e787c5717e0fb263b3915e11ca4f47624fe09c42
Parth Kale <164555314+Parkkyyy@users.noreply.github.com>
2024-09-11 14:39:06 +0530

    Add files via upload
    added files

---
+++ index.js
@@ -0,0 +1,40 @@
+// HINTS:
+// 1. Import express and axios
+
+// 2. Create an express app and set the port number.
+
+// 3. Use the public folder for static files.
+
+// 4. When the user goes to the home page it should render the index.ejs file.
+
+// 5. Use axios to get a random secret and pass it to index.ejs to display the
+// secret and the username of the secret.
+
+// 6. Listen on your predefined port and start the server.
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+    try {
+      const response = await axios.get("https://secrets-api.appbrewery.com/random");
+      const result = response.data;
+      console.log(result);
+      res.render("index.ejs", { data: result });
+    } catch (error) {
+      console.error("Failed to make request404:", error.message);
+      res.render("index.ejs", {
+        error: error.message,
+      });
+    }
+  });   
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
